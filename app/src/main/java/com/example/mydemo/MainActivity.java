85b237bbc4058148d63343475cb000088f479a31
Yinfx <450572249@qq.com>
2017-11-30 14:57:13 +0800

    333

--- app/src/main/java/com/example/mydemo/MainActivity.java
+++ app/src/main/java/com/example/mydemo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.mydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_native)
+    Button btn_native;
+    @BindView(R.id.btn_VasSonic)
+    Button btn_VasSonic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+
+    @OnClick({R.id.btn_native,R.id.btn_VasSonic})
+    public void click(View view){
+        switch (view.getId()){
+            case R.id.btn_native:
+                break;
+            case R.id.btn_VasSonic:
+                break;
+        }
+    }
+
 }
